00f28bccf10f25ad230a8da939717e579e76e1af
ktapia576 <ktapia576@hotmail.com>
2021-02-06 16:54:16 -0500

    Add WeaponScript and programatically add event listner to grabinteractbles

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class WeaponScript : MonoBehaviour
+{
+    private XRGrabInteractable interactable = null;
+
+    // Use Awake for getting components and cache preformance heavy methods
+    private void Awake()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        // Add event listner once interactable is picked up (enabled) and activated
+        interactable.activated.AddListener(Fire);
+    }
+
+    private void OnDisable()
+    {
+        // Remove event listener once interactable is disabled
+        interactable.activated.RemoveListener(Fire);
+    }
+
+    private void Fire(BaseInteractionEventArgs arg)
+    {
+        Debug.Log("Fire");
+        Debug.Log("Enabled: "+interactable.enabled); // debug whether enabled or not
+    } 
+}
