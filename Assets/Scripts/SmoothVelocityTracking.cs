cb7aa520b0ca8efb4eced34e3065dce68356b86f
ktapia576 <ktapia576@hotmail.com>
2021-02-17 03:15:31 -0500

    Add Smooth Velocity Script to smooth objects with velocity tracking

---
+++ Assets/Scripts/SmoothVelocityTracking.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SmoothVelocityTracking : MonoBehaviour
+{
+    private Rigidbody rigidBody = null; // Create var for rigidbody of interactable
+
+    private void Awake()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>(); // Get rigidBody of game object
+
+        rigidBody.maxAngularVelocity = 20f; // The default is 7 which is too low for fast movements (smoothes object when moving)
+    }
+}
