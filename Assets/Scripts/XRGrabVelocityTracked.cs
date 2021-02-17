576c62b3f898bffc902078b5d2f75f7bad36a197
ktapia576 <ktapia576@hotmail.com>
2021-02-17 00:48:23 -0500

    Change Fixed Time Step to 1/75 for physics to run at 75fps instead of 50fps(smoothing)

---
+++ Assets/Scripts/XRGrabVelocityTracked.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabVelocityTracked : XRGrabInteractable
+{
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        SetParentToXRRig();
+        base.OnSelectEntered(args);
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        SetParentToWorld();
+        base.OnSelectExited(args);
+    }
+
+    public void SetParentToXRRig()
+    {
+        transform.SetParent(selectingInteractor.transform);
+    }
+
+    public void SetParentToWorld()
+    {
+        transform.SetParent(null);
+    }
+}
